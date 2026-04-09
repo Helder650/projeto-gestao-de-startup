f24f7fa792901da937de0725c5266e1e5ac8182a
Helder650 <helderblm09@gmail.com>
2026-04-09 19:17:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function abrirDrive(materia) {
+
+const links = {
+
+matematica: "https://drive.google.com/drive/folders/10ZU8SxjYHfo6900_FC-8LLJjnb7FsL1K?usp=sharing",
+
+portugues: "https://drive.google.com/drive/folders/1ALDg7cNLitSLucZ0b_OEIYWqWQyHd0bl?usp=sharing",
+
+historia: "https://drive.google.com/drive/folders/1Da7WcwlfnYE6GOZT9mBz62EYQJz_yqui?usp=sharing",
+
+geografia: "https://drive.google.com/drive/folders/1k6WCUG21k1w9LbtymknQj6pZbWfWCdPR?usp=sharing",
+
+biologia: "https://drive.google.com/drive/folders/12wZdbSVQoZduNvv7F0u1dHfw6Aw1xwRs?usp=sharing",
+
+filosofia: "https://drive.google.com/drive/folders/1BP0N0pBCwE-fJUgzHMB2mGZxeY-cjg0K?usp=sharing",
+
+sociologia: "https://drive.google.com/drive/folders/1tdP7F6iI9e5DPRncwxJHEqRBpPR6Bb7g?usp=sharing"
+
+}
+
+window.open(links[materia], "_blank")
+
+}
